38be11ba5d8c093b0b4d9f8f92f9198ccb4a4d1f
Baoshuo <i@baoshuo.ren>
2026-08-14 19:33:13 +0800

    workspace: json formatter

--- packages/expo-modules-core/index.js
+++ packages/expo-modules-core/index.js
@@ -1,6 +1,3 @@
 'use strict';
 
-// This package intentionally owns no Expo Modules JavaScript API. Applications
-// import the canonical `expo-modules-core` package; RNOH autolinking consumes
-// the Harmony metadata and HAR published here.
 module.exports = Object.freeze({});
